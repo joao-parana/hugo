637be93ed51fba1c42ca4f1380d431f9c44cce95
Joao Antonio Ferreira <joao.parana@gmail.com>
2016-03-03 12:24:54 -0300

    Adicionando mensage de ajuda ao usuario

--- entrypoint.sh
+++ entrypoint.sh
@@ -2,10 +2,22 @@
 
 set -e 
 
-echo ". . . . HUGO Statice Web Site Generator . . . ."
+echo ". . . . HUGO Static Web Site Generator - versão 1.0.0 . . . ."
 echo "Você invocou com os seguintes parâmetros: $@"
 if [ "$1" = 'bash' ]; then
-    echo "Executando a bash"
+    echo 'Executando a bash.'
+    echo ' '
+    echo 'Você pode invocar HUGO como no exemplo abaixo:'
+    echo ' '
+    echo '/usr/bin/hugo \\'
+    echo '  --theme=hyde \\'
+    echo '  --buildDrafts \\'
+    echo '  --baseUrl="http://dockerhost.local" \\'
+    echo '  -d="/target"'
+    echo ' '
+    echo 'Lembre-se de colocar os fontes do site, (incluindo o tema usado) em /www '
+    echo 'Você pode usar Volumes para /www e para /target '
+    echo ' '
     /bin/bash $2 $3 $4 $5 $6 $7 $8 $9
     exit 0
 fi
